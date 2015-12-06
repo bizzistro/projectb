92dc8f9e5e94d10c819ff8ef588121b67f25c45a
Andrew Gremmo <andrewjgremmo@gmail.com>
2015-12-06 03:57:47 +0000

    Implement messaging system

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :posts
   root to: "sessions#new"
 
   resources :users, only: [:new, :create]
+  resources :messages, except: [:new, :create]
+  resources :posts do
+    resources :messages, only: [:new, :create]
+  end
   resource :session, only: [:new, :create, :destroy]
 end
